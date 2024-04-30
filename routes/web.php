8c81efa590e3ec859b50eaaea3384ae96195be92
Samuel Souza <samuelsouza0802@gmail.com>
2024-04-30 06:23:31 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('home')->group(function(){
     Route::get('/', [TarefasController::class,'cadastrarTarefas'])->name('home');
     Route::post('/', [TarefasController::class, 'cadastrarTarefas'])->name('home');
+    Route::put('/', [TarefasController::class, 'update'])->where('id', '[0-9]+')->name('home-update');
+   
    
 
 });
